15d37bb54c871d8a95ca55dc388e7d533620a077
Derek Wan <d.wan@icloud.com>
2026-01-03 22:48:25 +0000

    Regenerate `pull-request.yaml` with random sleep (#20)
    Reviewed-on: https://gitea.main:3000/qrt-public/conformalize/pulls/20
    Co-authored-by: Derek Wan <d.wan@icloud.com>
    Co-committed-by: Derek Wan <d.wan@icloud.com>

--- script.py
+++ script.py
@@ -46,7 +46,7 @@ if TYPE_CHECKING:
     from conformalize.types import StrDict
 
 
-__version__ = "0.1.20"
+__version__ = "0.1.21"
 LOGGER = getLogger(__name__)
 SECRETS_ACTION_TOKEN = "${{secrets.ACTION_TOKEN}}"  # noqa: S105
 
