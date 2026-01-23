f8f7ffb4653f8a90d9a9f4aad5db28b8de29a997
Derek Wan <derek@queensberryresearch.com>
2026-01-23 00:49:34 +0000

    Update hooks & dependencies (#76)
    Co-authored-by: github-actions-bot <noreply@github.com>
    Reviewed-on: https://gitea.main:3000/qrt/pre-commit-hooks/pulls/76

--- src/qrt_pre_commit_hooks/__init__.py
+++ src/qrt_pre_commit_hooks/__init__.py
@@ -7,7 +7,7 @@ from utilities.core import is_debug
 from utilities.logging import setup_logging
 from utilities.traceback import make_except_hook
 
-__version__ = "0.3.19"
+__version__ = "0.3.20"
 
 
 setup_logging(logger=__name__, files_dir=".logs")
