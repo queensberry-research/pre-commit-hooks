5ff5724aff2070bf9a44242e76ee9d726568cf14
Derek Wan <derek@queensberryresearch.com>
2026-02-01 10:55:50 +0000

    Add `log_version` to `set_up_logging` (#85)

--- src/qrt_pre_commit_hooks/__init__.py
+++ src/qrt_pre_commit_hooks/__init__.py
@@ -6,10 +6,10 @@ from utilities.constants import MONTH
 from utilities.core import is_debug, set_up_logging
 from utilities.traceback import make_except_hook
 
-__version__ = "0.3.28"
+__version__ = "0.3.29"
 
 
-set_up_logging(__name__, files=".logs")
+set_up_logging(__name__, files=".logs", log_version=__version__)
 sys.excepthook = make_except_hook(
     path_max_age=MONTH, path=".logs/errors", version=__version__, pudb=is_debug
 )
